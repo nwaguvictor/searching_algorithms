91242893aa009ca43def5d0be9b44d2d03d6f8b1
nwaguvictor <leofizzy3@gmail.com>
2022-01-04 23:07:15 +0100

    linear searching algorithm

---
+++ linear.js
@@ -0,0 +1,12 @@
+const linear = ({items = [], value}) => {
+  if (items.length === 0) return -1;
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] === value) return i;
+  }
+
+  return -1;
+}
+
+const res = linear({ items: [1, 2, 3, 4, 6], value: 20 });
+console.log(res)
